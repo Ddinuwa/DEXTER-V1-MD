bb7a7e2d5b9f37a7570ff3db334fdaa7aa68db1e
HESHAN-MD <ddinuwa83@gmail.com>
2025-01-11 22:16:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,26 +3,26 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '94761516805'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '94719845166'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'Sithumkalhara271@gmail.com'
 global.github = 'https://github.com/Sithuwa/SITHUWA-MD'
 global.location = 'Sri Lanka'
 global.gurl = 'https://instagram.com' // add your username
-global.sudo = process.env.SUDO || "94761516805"
-global.devs = '94761516805';
+global.sudo = process.env.SUDO || "94719845166"
+global.devs = '94719845166';
 global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/15b1dd8aeaa47888d75d7.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0xPTTFrYVJSQTJGQzdOdTdabmZwRTJ5bWhMTHBWVlExNU5tR1FSTEJGOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieTRFR1ZnamdoSGFMUCtoMFVBT0xsRzFMbndSWVdUbUtSdUY5VXJGQzJRWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhTGZkY1ZIUHgzajNzV2hGYXJreGVCM28xc3gzakZVZm5EcXQ3d1F1WjNJPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzL0RDMEsxRElRQzNhZjE4QXp1RmpVcWljTXR4b1hNUUQ5WGJ5TERCZVI4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNFUFgyV2tmWHZGcnNGUCs2MFVkYVhhTGs5cS8zZEtvOURaVHBTNlptRk09In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVTYzFhU3JpZXdaa052Njh2NkpScnpmcnN3aDRKNFZXVXZYQ2FUbkF6UW89In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0NpZHh0cGdQb3VGMS9pSE51Z0pXUzZveWFubFRMRlY2bnA5ak9pL2MwTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiczFBYUE0LzRFQmZteXh3TU5EVFllNEpEbjhNVjF5UXFsSCtKVE16NnZYZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNFdUxQc2I2Z3FuYU9JQzFORDY5cTF1d3JmYXIyQmJ4dzYwUlliSDdLZURNa3pnTmFFRnJpT0s1OGhZUyttWUdydWFtcmh5aUhmVTNmc1gwWFU3OUFnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjUyLCJhZHZTZWNyZXRLZXkiOiJENDVoMW95Q3NFeGZBd2NoeXRuYnhxdHZsSTlMS1Vib2hDVkVMbU5PTjI0PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJtVkJHSDYxelI3NlZLNlQtSnZzUENBIiwicGhvbmVJZCI6ImMyMTUwMDI2LTRmMmMtNGY5MC1iMjc5LWFkODVmNDgyMjc1ZiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJIcUJlem5iQUFpS2ZYbDVHYmF3UHlKRldtdXc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSjJPd29LNGx3NG0vaWl2Tzk1VXVXcjhIWWRzPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IllTOUU3REcxIiwibWUiOnsiaWQiOiI5NDcxOTg0NTE2NjoxNEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTnJOemY0R0VLVzVpcndHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiOGpOYldyaldCaXZHRHBSbTg2eDBFWXp3Q2lnS3FWWkJjRjljdEJmYURtUT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiOWkrZlhjTDlSRTlaTXhORUV0K0xtUGl2OHM1ci9uREFZNFU4d2tMeE5hWWQ1d0ZvQzFIMnd1aU1FbStsME5EQ3k4QlB3YlFWeElkTzArSklsOTN2Q0E9PSIsImRldmljZVNpZ25hdHVyZSI6ImN4clFDWEVYTXFxUmpkcDJSK21YM1FtSXU3T3Jhc2RQREgyWmhCdTROb3A0RDB2ZXlUSUlvZ1JWcHBFT294UXRYL0Rxb1JMbHUxRTcvTHROcG93R0RRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3MTk4NDUxNjY6MTRAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZkl6VzFxNDFnWXJ4ZzZVWnZPc2RCR004QW9vQ3FsV1FYQmZYTFFYMmc1ayJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTczNjYxMzA0MywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFJM0MifQ==",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'SITHUWA BOT',
   packname:  process.env.PACK_NAME || 'MADE BY SITHUM KALHARA',
   
   botname:   process.env.BOT_NAME === undefined ? "SITHUWA-MD" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'sithumkalhara' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
@@ -47,7 +47,7 @@ module.exports = {
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'sithuwa-md',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'privet' : process.env.WORKTYPE
 };
 
 
